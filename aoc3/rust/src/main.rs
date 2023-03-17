79aaa51065c063b253f61622977ce56809c4e94e
David Rebbe <1187684+ic3man5@users.noreply.github.com>
2023-03-17 01:22:24 -0400

    added comments and readme

--- aoc3/rust/src/main.rs
+++ aoc3/rust/src/main.rs
@@ -7,15 +7,19 @@ fn main() {
     let file = File::open("input.txt").unwrap();
     let reader = BufReader::new(file);
     let mut total_priority = 0u64;
-    let values = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let priority_list = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     for line in reader.lines() {
         if let Ok(rucksack_line) = line {
+            // Each rucksack has two compartments, each compartments have equal sizes
             let (first_compartment, second_compartment) = rucksack_line.split_at(rucksack_line.len()/2);
+            // Lets find the common characters in each
             let second_compartment_set: HashSet<char> = second_compartment.chars().collect();
             let common_rucksacks: Vec<char> = first_compartment.chars().filter(|c| second_compartment_set.contains(&c)).collect();
-            let priority = values.find(common_rucksacks[0]).unwrap() as u64 + 1;
+            // Lets find the priority of the letter:
+            // Lowercase item types a through z have priorities 1 through 26.
+            // Uppercase item types A through Z have priorities 27 through 52.
+            let priority = priority_list.find(common_rucksacks[0]).unwrap() as u64 + 1;
             total_priority += priority;
-            //println!("common: {first_compartment} {second_compartment} {common_rucksacks:?} {priority} {total_priority}");
         }
     }
     println!("Total Priority: {total_priority}");
